19d66c11e494a5b9d8f966ed0206ce4d912e541c
leszq <lestasiak@gmail.com>
2014-04-03 22:41:38 +0200

    serwer: aktualizacja pliku connectionRoute

--- server/NodeTest/routes/connectionRoute.js
+++ server/NodeTest/routes/connectionRoute.js
@@ -1,6 +1,7 @@
 ﻿var qr = require('qr-image');
 var fs = require('fs');
 
+var Success = require('../results/result.js').Success;
 var User = require('../models/user.js');
 var Room = require('../models/room.js');
 var Sequelize = require('sequelize');
@@ -13,5 +14,10 @@ module.exports.HelloWorld = function(req, res) {
         res.send("Server v0.2");
     else
         res.send("Witaj nr: "+ req.session.user);
+};
+
+module.exports.Ping = function(req, res) {
+
+    res.end(new Success("PONG").JSON());
 };
 
