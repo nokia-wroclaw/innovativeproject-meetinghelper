69d2527fb29d0bbe7fecd42efea6f82adb07c3f2
Mateusz <mateuszpiorko7@gmail.com>
2014-04-06 18:44:59 +0200

    added: showing data on wall

--- client/meeting_helper/www/js/main.js
+++ client/meeting_helper/www/js/main.js
@@ -1,3 +1,20 @@
+var t_serverData = {
+	totalRecords: 0,
+	data: []
+};
+
+var t_myData = {
+        data: []
+};
+
+var t_onlineUser = {
+	numberOfOnlineUsers: 0,
+        user: []
+	/*id: [],
+	name: [],
+	surname: [],
+	login: []*/
+};
 
 /**
  * Funkcje, od których oczekujemy odpowiedzi, wywołujemy,
@@ -14,10 +31,11 @@ var main = {
 		devices.camera.takePicture(quality, function(imageSrc) {
 			connection.file.upload.photo(imageSrc);
 
-			var image = document.getElementById('myImageCamera');
-			image.style.display = 'block';
+                        main.addNewMyData('image', imageSrc);
+			//var image = document.getElementById('myImageCamera');
+			//image.style.display = 'block';
 
-			image.src = imageSrc/*"data:image/jpeg;base64," + */;
+			//image.src = imageSrc/*"data:image/jpeg;base64," + */;
 		});
 	},
 
@@ -137,32 +155,124 @@ var main = {
 				connection.socket.enterRoom(roomId);
 			});
 		});
-	}
-};
+	},
+        
+        /**
+	 * Dodawanie elementu do struktury JSON
+	 * @param {String} type
+         * @param {String} data
+         * @param {String} author
+	 */
+	addNewServerData: function(type, data, author) {
+		t_serverData.totalRecords += 1;
+		t_serverData.data[t_serverData.totalRecords-1] = [type, data, author];
+                main.updateServerData(type, data, author);
+	},
+        
+        /**
+         * 
+         * @param {String} type
+         * @param {String} data
+         */
+        addNewMyData: function(type, data) {
+                t_myData.data[t_myData.data.length] = [type, data];
+                main.updateMyData(type, data);
+        },
+        
+        /**
+         * 
+         * @returns {undefined}
+         */
+        showServerData: function() {
+                for (var data in dataTable) {
+                    ;
+                }
+        },
+        
+        /**
+         * 
+         * @param {String} type
+         * @param {String} data
+         * @param {String} author
+         */
+        updateServerData: function(type, data, author) {
+                var image = document.createElement("img");
+                var node = document.createAttribute("alt");
+                node.value="photo";
+                image.setAttributeNode(node);
+                node = document.createAttribute("style");
+                node.value="display:none;width:90%;margin-left:5%;";
+                image.setAttributeNode(node);
+                var element = document.getElementById('received');
+                element.appendChild(image);
+                var tmp = element.getElementsByTagName("img")[t_serverData.data.length-1];
+                tmp.style.display='block';
+                tmp.src=data;
+        },
 
-/**
- * Funkcja wywoływana po poprawnym ustawieniu strony.
- */
-connection.onSetUrl = function() {
-	//
+        /**
+         * 
+         * @param {String} type
+         * @param {String} data
+         */
+        updateMyData: function(type, data) {
+                var image = document.createElement("img");
+                var node = document.createAttribute("alt");
+                node.value="photo";
+                image.setAttributeNode(node);
+                node = document.createAttribute("style");
+                node.value="display:none;width:60px;height:60px;";
+                image.setAttributeNode(node);
+                var element = document.getElementById('myImageCamera');
+                element.appendChild(image);
+                var tmp = element.getElementsByTagName("img")[t_myData.data.length-1];
+                tmp.style.display='block';
+                tmp.src=data;
+        },
+        
+	/**
+	 * Dodawanie nowego użytkownika do spotkania
+         * @param {Number} id_
+	 * @param {String} name_
+         * @param {String} surname_
+         * @param {String} login_
+	 */
+	userEntered: function(id_, name_, surname_, login_/*meaby few more params*/) {
+		t_onlineUser.numberOfOnlineUsers += 1;
+                t_onlineUser.user[t_onlineUser.numberOfOnlineUsers-1] = [id_, name_, surname_, login_];
+                //onlineUserTable.id = id_;
+		//onlineUserTable.id = onlineUserTable.id[numberOfOnlineUsers-1];
+		//onlineUserTable.name = name_;
+		//onlineUserTable.surname = surname_;
+		//onlineUserTable.login = login_;
+	},
+        
+        /**
+         * 
+         * @param {Number} id_
+         */
+        userLeft: function(id_) {
+                ;
+        }
 };
 
 /**
  * Elementy otrzymane od websocketa obecnie obsługuje się u nas w ten sposób:
  */
 connection.socket.receive.onNewPhoto = function(data) {
-	var image = document.getElementById('received');
+        main.addNewServerData('image', data, 'sbd');
+	/*var image = document.getElementById('received');
 
 	image.style.display = 'block';
-	image.src = data.data;
+	image.src = data.data;*/
 };
 
 /**
  * Elementy otrzymane od websocketa obecnie obsługuje się u nas w ten sposób:
  */
-connection.socket.receive.onNewUser = function(data) {
+/*connection.socket.receive.onNewUser = function(data) {
 	callback(data);
-};
+};*/
 
 /**
  * Elementy otrzymane od websocketa obecnie obsługuje się u nas w ten sposób:
