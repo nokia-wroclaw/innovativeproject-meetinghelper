adb3543dac4a1f3fd6a1ba1af4f9efebfb43232e
leszq <lestasiak@gmail.com>
2014-05-13 16:33:04 +0200

    Server: Dodanie komentarzy do pliku server.js

--- server/NodeTest/server.js
+++ server/NodeTest/server.js
@@ -64,43 +64,181 @@ app.io.set('authorization', function(data, accept){
 
 app.io.on('connection', connection.Connected);
 
+/*
+* Requirement: none
+* callback event: pong
+*/
+
 app.io.route('ping', connection.SocketPing);
+
+/*
+* Description: Get users online
+* Requirement: logged, in room
+* callback event: usersOnline ((userID, name)[])
+*     
+*/
 app.io.route('users/online', user.UsersOnline); 
+
+/*
+* Description: Enter to meeting
+* Requirement: logged
+* callback event: joined (meetingID, name)
+* hidden result: in room
+*/
 app.io.route('meetings/enterMeeting', meeting.EnterMeeting);
 
+
+/*
+* Requirement: none
+* return: server version
+*/
 app.get('/api/', connection.HelloWorld);
+
+/*
+* Requirement: none
+* return: PONG
+*/
 app.get('/api/ping', connection.Ping);
 
+/*
+* Description: Create Meeting
+* Requirement: logged
+* POST arguments:
+*       - meetingName (string)
+* return:         
+*       - created meeting (object)
+*/
 app.post('/api/meetings/create', user.IsLogin, meeting.CreateRoom);
+
+/*
+* Description: Join to Meeting
+* Requirement: logged
+* POST arguments:
+*       - meetingID (integer)
+* return:         
+*       - result (boolean)
+*/
 app.post('/api/meetings/join', user.IsLogin, meeting.JoinRoom);
+
+/*
+* Description: Join to Meeting by Code
+* Requirement: logged
+* POST arguments:
+*       - accessCode (string)
+* return:         
+*       - result (boolean)
+*/
 app.post('/api/meetings/joinByCode', user.IsLogin, meeting.JoinRoomByCode);
+
+/*
+* Description: Get joined meeting
+* Requirement: logged
+* GET arguments:
+*       - none
+* return:         
+*       - result (boolean)
+*/
 app.get('/api/meetings/list', user.IsLogin, meeting.GetRoomsList);
 
+
+/*
+* Description: Get qrcode
+* Requirement: none
+* GET arguments:
+*       - none
+* return:         
+*       - qrCode (svg)
+*/
 app.get('/api/qrcode', qrcode.QRCode);
+
+/*
+* Description: Generate qrcode
+* Requirement: none
+* GET arguments:
+*       - address name(string)
+*       - port name (integer)
+* return:         
+*       - qrCode (svg)
+*/
 app.get('/api/qrcode/:address/:port', qrcode.QRCodeGenerator);
 app.get('/api/qrcode/:groupCode', qrcode.QRCodeJoinGroup);
 
-
+/*
+* Description: Login
+* Requirement: none
+* POST arguments:
+*       - login(string)
+*       - password(string)
+* return:         
+*       - user(object)
+*/
 app.post('/api/login', user.Login);
-app.post('/api/register', user.Register);
-app.get('/api/logout',  user.Logout);
 
+/*
+* Description: Register
+* Requirement: none
+* POST arguments:
+*       - login(string)
+*       - password(string)
+* return:         
+*       - userID(integer)
+* hidden result: logged
+*/
+app.post('/api/register', user.Register);
+
+/*
+* Description: Logout
+* Requirement: logged
+* GET arguments:
+*       - none
+* return:         
+*       - result(boolean)
+*/
+app.get('/api/logout',user.IsLogin, user.Logout);
+
+/*
+* Description: Get All Meterials from meeting
+* Requirement: logged, in meeting
+* GET arguments:
+*       - none
+* return:         
+*       - materials[] (array of materials)
+*/
 app.get('/api/materials/getAll', user.IsLogin, meeting.IsRoom, material.GetAll);
+
+/*
+* Description: Send file to meeting
+* Requirement: logged, in meeting
+* POST arguments:
+*       - file(file)
+* return:         
+*       - newfilename (string)
+*/
 app.post('/api/materials/sendFile', user.IsLogin, meeting.IsRoom, material.SendFile);
+
+/*
+* Description: Send file to meeting
+* Requirement: logged, in meeting
+* GET arguments:
+*       - materialID(integer)
+* return:         
+*       - file (file)
+*/
 app.get('/api/materials/file/:materialID', user.IsLogin, meeting.IsRoom, material.DownloadFile);
 
+
+/*
+* Description: Test login
+*/
 app.get('/api/reg', function(req, res) {
     	
     res.writeHead(200, {'Content-Type': 'text/html' });
 	res.end(form1);
 });
 
-app.get('/api/file', function(req, res) {
-    	
-    res.writeHead(200, {'Content-Type': 'text/html' });
-	res.end(form);
-});
-
+/*
+* Description: Test generate database
+*/
 app.get('/api/generateDatabase', function(req, res) {
    sequelize
   .sync({ force: true })
@@ -111,26 +249,6 @@ app.get('/api/generateDatabase', function(req, res) {
 });
 
 
-app.get('/api/photoExist/:photoName', function(req, res) {
-    var photoName = req.params.photoName;
-    
-    fs.exists('./uploads/'+photoName+'.jpg', function (exists) {
-        if(exists){
-            res.send("Plik istnieje");
-        }
-        else{
-            res.send("Plik nie istnieje");
-        }
-    });
-});
-
-app.get('/api/user/:filename', function(req, res){
-  var uid = req.params.filename;
-  res.sendfile('./uploads/'+uid);
-});
-
 app.listen(Config.port, function() { }); 
-
-
 module.exports = app;
 
