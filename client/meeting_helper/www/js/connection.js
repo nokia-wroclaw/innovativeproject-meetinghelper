e9c17e966d3b41dbd6363ac6c0c97bb75a643527
MichalVVV <malinka.michal@gmail.com>
2014-06-16 16:52:09 +0200

    client: poprawienie szyfrowania - brak tostring

--- client/meeting_helper/www/js/connection.js
+++ client/meeting_helper/www/js/connection.js
@@ -319,7 +319,7 @@ var connection = {
 			connection.action._base(
 				connection.action.types.post,
 				connectionLinks.post.login,
-				{login: login, password: CryptoJS.MD5(password)},
+				{login: login, password: CryptoJS.MD5(password).toString()},
 				connection.receive.onLogin(callb),
 				errorCallb);
 		},
@@ -340,7 +340,7 @@ var connection = {
 				connection.action._base(
 					connection.action.types.post,
 					connectionLinks.post.register,
-					{login: login, password: CryptoJS.MD5(password)},
+					{login: login, password: CryptoJS.MD5(password).toString()},
 					connection.receive.onRegister(callb),
 					errorCallb);
 			} else {
